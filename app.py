56c547113ca7ec067520d7a06deafd678a8dc674
Tafen Ngassa Vianela Ingride <vianelatafen237@gmail.com>
2026-01-08 15:07:21 +0100

    Fix app.run for production deployment

--- app.py
+++ app.py
@@ -2267,4 +2267,5 @@ def generate_attestation_pdf(data, attestation_id):
 # ==================== LANCEMENT DE L'APPLICATION ====================
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
